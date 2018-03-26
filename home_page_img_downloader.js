bd795ecffabc200687f6fcd7cae3440577b85bc0
Hrant Alaverdyan <dev.hrant@gmail.com>
2018-03-26 15:05:25 +0400

    Update home_page_img_downloader.js

--- home_page_img_downloader.js
+++ home_page_img_downloader.js
@@ -1,24 +1,15 @@
-// Home
-// var activity_items = [];
-// activity_items = document.getElementsByClassName("activity_item");
-// alert(activity_items.length);
-
 window.addEventListener('load', function () {
-
-    // alert("Loaded");
 
     var btnDivs = document.getElementsByClassName("photo_activity_item__details_top_right");
     var imageDivs = document.getElementsByClassName("photo_activity_item__img");
     var img_src = "";
     for (var i = 0; i < btnDivs.length; i++) {
         img_src = imageDivs[i].src;
-        // position: absolute;
+        
         var downloadBtnTag = "<a style='vertical-align: middle;font-size: 14px;margin-left: 10px;display: inline-block;padding-right: 10px;' class='extra-actions' href='" + img_src + "' download><p align='center' style='background-color: #34bf49; width: 30px; height: 30px; display: table-cell;vertical-align: middle;color: #ffffff; border-radius: 3px;'>D</p></a>";
 
         var tempHtml = btnDivs[i].innerHTML;
         btnDivs[i].innerHTML = downloadBtnTag + tempHtml;
     }
-
-
 });
 
